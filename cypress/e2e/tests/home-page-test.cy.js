d16bbc79be68382ca37838728d2fd9806f78f7fb
pillika <ugne.ciz@gmail.com>
2024-01-08 00:20:40 +0200

    Url assertions added

--- cypress/e2e/tests/home-page-test.cy.js
+++ cypress/e2e/tests/home-page-test.cy.js
@@ -9,41 +9,43 @@ describe("home-page-test", function () {
     })
     xit("search for restaurant that delivers burgers to kauno dokas", function () {
         cy.clickDataLocalizationKeyButton("gdpr-consents.banner.accept-button");
-        cy.get('#front-page-input').type("Kauno Dokas");
+        cy.get('[data-test-id="address-picker-input.input"]').type("Kauno Dokas");
         cy.get('#suggestions', { timeout: 10000 }).should('be.visible');
-        cy.get('#front-page-input').type('{enter}');
+        cy.get('[data-test-id="address-picker-input.input"]').type('{enter}');
         cy.url().should('eq', 'https://wolt.com/en/discovery');
         cy.get(`[data-test-id="header.address-select-button.address-text"]`).contains('Jonavos gatvė 7');
         //need to improve selector
         cy.get(':nth-child(2) > .sc-147d0703-0')
         .click();
         cy.get('h1').should('have.text', "Restaurants near me");
+        cy.url().should('eq', 'https://wolt.com/en/discovery/restaurants');
         cy.contains('Burger').click();
         cy.get('h1').should('have.text', "Burger near me");
+        cy.url().should('eq', 'https://wolt.com/en/discovery/category/burgers');
         cy.get(`[data-test-id="VenueVerticalListGrid"]>a`).its('length').should('be.gt', 0);
     })
     xit("select favorite burger and add it to chart", function () {
         cy.clickDataLocalizationKeyButton("gdpr-consents.banner.accept-button");
-        cy.get('#front-page-input').type("Kauno Dokas");
+        cy.get('[data-test-id="address-picker-input.input"]').type("Kauno Dokas");
         cy.get('#suggestions', { timeout: 10000 }).should('be.visible');
-        cy.get('#front-page-input').type('{enter}');
+        cy.get('[data-test-id="address-picker-input.input"]').type('{enter}');
         //need to improve selector
         cy.get(':nth-child(2) > .sc-147d0703-0')
             .click();
         cy.contains('Burger').click();
         cy.get('[data-test-id="venueCard.sventas-dumas"]').click();
-        cy.contains('420 Blaze Burger with CBD').click();
+        cy.contains('420 Blaze Burger with CBD').click({force: true});
         cy.get('div[data-modal-content-container="true"] h2')
             .should('have.text', "420 Blaze Burger with CBD");
         cy.get('[data-test-id="product-modal.submit"]').click();
         cy.get('[data-test-id="cart-view-button"]').should('exist');
 
     })
-    it("check if the burger is added to the chart correctly", function () {
+    xit("check if the burger is added to the chart correctly", function () {
         cy.clickDataLocalizationKeyButton("gdpr-consents.banner.accept-button");
-        cy.get('#front-page-input').type("Kauno Dokas");
+        cy.get('[data-test-id="address-picker-input.input"]').type("Kauno Dokas");
         cy.get('#suggestions', { timeout: 10000 }).should('be.visible');
-        cy.get('#front-page-input').type('{enter}');
+        cy.get('[data-test-id="address-picker-input.input"]').type('{enter}');
         //need to improve selector
         cy.get(':nth-child(2) > .sc-147d0703-0')
             .click();
